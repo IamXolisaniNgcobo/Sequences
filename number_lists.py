70ea44599d9cdd2745e3ccc1e4bd632a75b21d6b
XOLISANI NGCOBO <38984822+Xolsann722@users.noreply.github.com>
2021-02-28 22:53:46 +0200

    Update number_lists.py

--- number_lists.py
+++ number_lists.py
@@ -1,14 +1,23 @@
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-print(min(even))
-print(max(even))
+even.extend(odd)
+print(even)
+another_even = even
+print(another_even)
 
-print(min(odd))
-print(max(odd))
+even.sort(reverse=True)
+print(even)
+print(another_even)
 
-print()
-print(len(even))
-print(len(odd))
-print()
-print("mississipi".count("issi"))
+# print(min(even))
+# print(max(even))
+#
+# print(min(odd))
+# print(max(odd))
+#
+# print()
+# print(len(even))
+# print(len(odd))
+# print()
+# print("mississipi".count("issi"))
