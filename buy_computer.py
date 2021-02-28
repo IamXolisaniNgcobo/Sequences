8369f9b0bc667cfbddd6e044827dd446c00d97a1
XOLISANI NGCOBO <38984822+Xolsann722@users.noreply.github.com>
2021-02-28 22:53:11 +0200

    Update buy_computer.py

--- buy_computer.py
+++ buy_computer.py
@@ -2,30 +2,31 @@ available_parts = ["computer",
                    "monitor",
                    "keyboard",
                    "mouse",
-                   "mouse mat",
-                   "hdmi cable",
-                   "dvd drive"]
 
+                   "hdmi cable",
+                   "dvd drive"
+                   ]
+
+# valid_choices = [str(i) for i in range(1, len(available_parts) + 1)]
+valid_choices = []
+for i in range(1, len(available_parts) + 1):
+    valid_choices.append(str(i))
+print(valid_choices)
 current_choice = "-"
-computer_parts = []     # create an empty list
+computer_parts = []  # create an empty list
 
 while current_choice != "0":
-    if current_choice in "123456":
-        print("Adding {}".format(current_choice))
-        if current_choice == "1":
-            computer_parts.append("computer")
-        elif current_choice == "2":
-            computer_parts.append("monitor")
-        elif current_choice == "3":
-            computer_parts.append("keyboard")
-        elif current_choice == "4":
-            computer_parts.append("mouse")
-        elif current_choice == "5":
-            computer_parts.append("mouse mat")
-        elif current_choice == "6":
-            computer_parts.append("hdmi cable")
-        elif current_choice == "7":
-            computer_parts.append("dvd drive")
+    if current_choice in valid_choices:
+        index = int(current_choice) - 1
+        chosen_part = available_parts[index]
+        if chosen_part in computer_parts:
+            # it already in the list, so remove it
+            computer_parts.remove(chosen_part)
+        else:
+            print("Adding {}".format(current_choice))
+            computer_parts.append(chosen_part)
+        print("Your list contains {}".format(computer_parts))
+
 
     else:
         print("Please add options from the list below: ")
