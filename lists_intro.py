23dc83a3532c2ae01ddf389846fb276aedc9ecae
XOLISANI NGCOBO <38984822+Xolsann722@users.noreply.github.com>
2021-03-07 10:51:10 +0200

    Create lists_intro.py

---
+++ lists_intro.py
@@ -0,0 +1,21 @@
+computer_parts = ["cumputer",
+                  "monitor",
+                  "keyboard",
+                  "mouse",
+                  "mouse mat"
+                  ]
+print(computer_parts)
+# computer_parts[3] = "trackball"
+print(computer_parts[3:])
+
+computer_parts[3:] = ["trackball"]
+print(computer_parts)
+
+# for part in computer_parts:
+#     print(part)
+#
+# print()
+# print(computer_parts[2])
+#
+# print(computer_parts[0:3])
+# print(computer_parts[-1])
