268738d723c33edcec7556d475ae60cbef1a6b25
XOLISANI NGCOBO <38984822+Xolsann722@users.noreply.github.com>
2021-03-07 10:50:31 +0200

    Create outliers.py

---
+++ outliers.py
@@ -0,0 +1,27 @@
+data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
+        160, 170, 183, 185, 187, 188, 191, 350, 360]
+# mutable sequence types docs.python.org/3/library/stdtypes.html#mutables-sequence-types
+min_valid = 100
+max_valid = 200
+
+# process the low values in the list
+stop = 0
+for index, value in enumerate(data):
+    if value >= min_valid:
+        stop = index
+        break
+
+print(stop)
+del data[:stop]
+print(data)
+
+# process the high values in the list
+start = 0
+for index in range(len(data) - 1, -1, -1):
+    if data[index] <= max_valid:
+        start = index
+        break
+
+print(start)  # for debugging
+del data[start + 1:]
+print(data)
