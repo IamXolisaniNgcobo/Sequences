75c6c11293c52223626e49305ca3dc90a8d46646
XOLISANI NGCOBO <38984822+Xolsann722@users.noreply.github.com>
2021-03-07 11:25:22 +0200

    Update outliers.py

--- outliers.py
+++ outliers.py
@@ -1,5 +1,14 @@
-data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
+# data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
+#         160, 170, 183, 185, 187, 188, 191, 350, 360]
+# data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
+#         160, 170, 183, 185, 187, 188, 191]
+data = [104, 105, 110, 120, 130, 130, 150,
         160, 170, 183, 185, 187, 188, 191, 350, 360]
+# data = [104, 105, 110, 120, 130, 130, 150,
+#         160, 170, 183, 185, 187, 188, 191]
+# data = [1041, 1051, 1101, 1201, 1301, 1301, 1501,
+#         1601, 1701, 1831, 1851, 1871, 1881, 1911]
+data=[]
 # mutable sequence types docs.python.org/3/library/stdtypes.html#mutables-sequence-types
 min_valid = 100
 max_valid = 200
@@ -19,9 +28,12 @@ print(data)
 start = 0
 for index in range(len(data) - 1, -1, -1):
     if data[index] <= max_valid:
-        start = index
+        # We have the index of the last item to keep the
+        # Set 'start' to the position of the first
+        # item to delete which is 1 after 'index'.
+        start = index + 1
         break
 
 print(start)  # for debugging
-del data[start + 1:]
+del data[start:]
 print(data)
