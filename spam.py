2555cb4c5a7372584dee22c6156c2a9724a6276c
XOLISANI NGCOBO <38984822+Xolsann722@users.noreply.github.com>
2021-03-14 15:23:07 +0200

    Update spam.py

--- spam.py
+++ spam.py
@@ -5,8 +5,8 @@ menu = [
     ["egg", "bacon", "spam"],
     ["egg", "bacon", "sausage", "spam"],
     ["spam", "bacon", "sausage", "spam"],
-    ["spam", "egg", "spam", "spam", "bacon", "spam"],
     ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon", "spam"],
 ]
 
 for meal in menu:
