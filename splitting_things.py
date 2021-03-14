e78a0d31f505b8437b383ce23905908d26d26ec6
XOLISANI NGCOBO <38984822+IamXolisaniNgcobo@users.noreply.github.com>
2021-03-14 16:18:18 +0200

    Update splitting_things.py

--- splitting_things.py
+++ splitting_things.py
@@ -5,3 +5,22 @@ print(words)
 
 numbers = "9,223,372,036,854,775,807"
 print(numbers.split(","))
+
+generated_list = ['9', ' ',
+                  '2', '2', '3', ' ',
+                  '3', '7', '2', ' ',
+                  '0', '3', '6', ' ',
+                  '8', '5', '4', ' ',
+                  '7', '7', '5', ' ',
+                  '8', '0', '7']
+values = "".join(generated_list)
+print(values)
+
+values_list = values.split()
+print(values_list)
+
+new_values_list = []
+for item in values_list:
+    int(item)
+    new_values_list.append(int(item))
+print(new_values_list)
