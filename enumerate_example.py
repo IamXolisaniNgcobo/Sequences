3dbb8fa7bf701899c03b7fcc648819673d1d73ed
XOLISANI NGCOBO <38984822+IamXolisaniNgcobo@users.noreply.github.com>
2021-04-11 23:02:00 +0200

    Update enumerate_example.py

--- enumerate_example.py
+++ enumerate_example.py
@@ -1,9 +1,9 @@
 # for index, character in enumerate("abacdefgh"):
 #     print(index, character) #enumerate returns character with index
 for t in enumerate("abcdefgh"):
-    index,character = t
-    print(index,character)
+    index, character = t
+    print(index, character)
 
-index,character = (0, 'a')
+index, character = (0, 'a')
 print(index)
 print(character)
